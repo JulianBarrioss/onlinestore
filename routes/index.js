199c9d9aa0ec8913e864c27c1105fd03e7d2c4bc
Julian Barrios <julianbarrios0821@gmail.com>
2021-12-01 18:25:52 -0600

    getting commands from the profile

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const usersRouter = require('./users.router');
 const orderRouter = require('./orders.router');
 const customersRouter = require('./customers.router');
 const authRouter = require('./auth.router');
+const profileRouter = require('./profile.router');
+
 
 function routerApi(app) {
   const router = express.Router();
@@ -16,6 +18,8 @@ function routerApi(app) {
   router.use('/orders', orderRouter);
   router.use('/customers', customersRouter);
   router.use('/auth', authRouter);
+  router.use('/profile', profileRouter);
+
 }
 
 module.exports = routerApi;
